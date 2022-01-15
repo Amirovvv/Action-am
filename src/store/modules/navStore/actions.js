d72dfe99a2219b290bea6969811b5ef03271c4ae
Amirovvv <a.amirovv@mail.ru>
2022-01-15 22:19:01 +0400

    feat: get all pages

--- src/store/modules/navStore/actions.js
+++ src/store/modules/navStore/actions.js
@@ -1,4 +1,11 @@
-import { db, doc, auth, collection, setDoc } from '@/firebase/firebaseConfig'
+import {
+	db,
+	doc,
+	auth,
+	collection,
+	setDoc,
+	onSnapshot,
+} from '@/firebase/firebaseConfig'
 
 export default {
 	async setPage({ commit }, type) {
@@ -8,6 +15,22 @@ export default {
 				collection(db, 'users/' + auth.currentUser.uid + '/pages'),
 			)
 			setDoc(page, { name: 'view', id: page.id, type: type })
+		} catch (e) {
+			console.log(e)
+		}
+	},
+
+	async getPages({ commit }) {
+		commit('CLEAR_PAGES_DATA')
+		try {
+			onSnapshot(
+				collection(db, 'users/' + auth.currentUser.uid + '/pages'),
+				(sn) => {
+					sn.docs.map((doc) => {
+						commit('SET_PAGES_DATA', doc.data())
+					})
+				},
+			)
 		} catch (e) {
 			console.log(e)
 		}
